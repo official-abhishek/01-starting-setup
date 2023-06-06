c9a42a651815445ea585196e5793319351f5b395
official-abhishek <135330349+official-abhishek@users.noreply.github.com>
2023-06-06 08:09:53 +0530

    State and Events introduction

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,15 +1,29 @@
-import "./ExpenseItem.css";
-function ExpenseItem(props) {
+import React, {useState} from 'react' ;
+import "./ExpenseItem.css"; 
+
+import ExpenseDate from "./ExpenseDate";
+const ExpenseItem = (props) => {
+  const [title, setTitle] = useState(props.title) ;
+  const clickHandler = () => {
+    setTitle('Updated !') ;
+    console.log('Clicker !!!') ;
+  }
+  
   
   return (
+    <div>
     <div className="expense-item">
-      <div>{(props.date).toISOString()}</div>
+      <ExpenseDate date={props.date} />
       <div className="expense-item__description">
-        <h2>{props.title}</h2>
+        <h2>{title}</h2>
       </div>
       <div className="expense-item__price">${props.amount}</div>
+      <button onClick={clickHandler} >Change Title</button>
+    </div> 
+    
     </div>
   );
+  
 }
 
 export default ExpenseItem;
